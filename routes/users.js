02c7d2c43c530194b91ce8a2272174d02a095515
Shreyansh kankane <shreyanshguptaphy@gmail.com>
2023-06-21 22:09:11 +0530

    change user

--- routes/users.js
+++ routes/users.js
@@ -9,8 +9,8 @@ import { verifyToken } from "../middleware/auth.js";
 const router = express.Router();
 
 /* READ */
-router.get("/:id", verifyToken, getUser);
-router.get("/:id/friends", verifyToken, getUserFriends);
+router.get("/:id", verifyToken,getUser);
+router.get("/:id/friends",verifyToken,getUserFriends);
 
 /* UPDATE */
 router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
